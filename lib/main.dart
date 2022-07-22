e6eae53847606fe39a12f8bb252321f5adcd56fc
Awaizkazi <88947135+Awaizkazi@users.noreply.github.com>
2022-07-22 19:09:41 +0530

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:loginpage/login.dart';
+import 'package:loginpage/register.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      debugShowCheckedModeBanner: false,
+      initialRoute: 'login',
+      routes: {
+        'login': (context) => MyLogin(),
+        'register': (context) => MyRegister(),
+      },
+    ),
+  );
+}
